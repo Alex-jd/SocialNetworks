a6cae8a3e914a5f57a60e348616a2646fa7cddeb
alex_jd <twoalexan@mail.ru>
2017-03-22 23:24:44 +0500

    create the new class. Select the Set Cover Problem direction.

---
+++ src/graph/SetCoverSol.java
@@ -0,0 +1,53 @@
+package graph;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeSet;
+
+import util.GraphLoader;
+
+public class SetCoverSol {
+
+	private Map<Integer, ArrayList<Integer>> adjListsMap;
+	private TreeSet<Integer> cover;
+	private int numOfVertices = 0;
+
+	public SetCoverSol() {
+		Graph g = new CapGraph();
+		GraphLoader.loadGraph(g, "data/twitter_combined.txt");
+		this.adjListsMap = g.getMatrix();
+		this.setTheCover();
+	}
+
+
+	public void setTheCover() {
+		cover = new TreeSet<Integer>();
+		for (Map.Entry<Integer, ArrayList<Integer>> entry : adjListsMap.entrySet()) {
+			if (!cover.contains(entry.getKey())) {
+				cover.add(entry.getKey());
+				numOfVertices++;
+			}
+			for (int i : entry.getValue()) {
+				if (!cover.contains(i)) {
+					cover.add(i);
+					numOfVertices++;
+				}
+			}
+		}
+	}
+
+	public int getNumOfVertices() {
+		return numOfVertices;
+	}
+
+	public TreeSet<Integer> getTheCover() {
+		return cover;
+	}
+
+	public static void main(String[] args) {
+		SetCoverSol coverTest = new SetCoverSol();
+		System.out.println(coverTest.getTheCover());
+		System.out.println(coverTest.getNumOfVertices());
+	}
+
+}
