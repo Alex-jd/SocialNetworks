c5aa02d2c5121f68776ab777013558ae8d3e74d5
alex_jd <twoalexan@mail.ru>
2017-03-30 18:23:24 +0500

    continue to implement

--- src/SCPS/SCPS.java
+++ src/SCPS/SCPS.java
@@ -30,19 +30,59 @@ public class SCPS {
 		Set<Integer> universe = locGraph.getUnivers();
 		// While coverU not equal universe continue
 		Queue<Integer> toExplore = new LinkedList<Integer>(universe);
+		int next = toExplore.poll();// Get the next vertex
 		while (!isEquals(coverU, universe)) {
-			final int next = toExplore.poll();
+			// Get the edges of the next vertex
+			Integer bestNode = getTheBestNode(next, toExplore, coverU);
 			final Set<Integer> edgesCurNode = locGraph.getEdgesSort(next);
-
-
+			getTheUnicEdges(edgesCurNode, coverU);
+			toExplore.remove((int) 100500);// remove(new Integer(100500))
+			next = toExplore.peek();// Get the next vertex
 		}
-
 
 		return minDomSet;
 	}
 
+	private Integer getTheBestNode(Integer next, Queue<Integer> toExplore, Set<Integer> coverU) {
+		Integer theBestNode = next;
+		// getTheUnicEdges(locGraph.getEdgesSort(next), coverU).size();
+		final Iterator<Integer> nextToExplore = toExplore.iterator();
+		int bestSize = getTheUnicEdges(locGraph.getEdgesSort(next), coverU).size();
+		while (nextToExplore.hasNext()) {
+			next = nextToExplore.next();
+			int nextSize = getTheUnicEdges(locGraph.getEdgesSort(nextToExplore.next()), coverU).size();
+			if (bestSize < nextSize) {
+				bestSize = nextSize;
+				theBestNode = next;
+			}
+		}
+		return theBestNode;
+	}
+
+	private Set<Integer> getTheUnicEdges(Set<Integer> edgesCurNode, Set<Integer> coverU) {
+		final Set<Integer> unicEdges;
+		if (coverU == null) {
+			unicEdges = edgesCurNode;
+			return unicEdges;
+		} else if (edgesCurNode == null) {
+			return null;
+		} else {
+			unicEdges = new TreeSet<Integer>();
+			final Iterator<Integer> curNodeIter = edgesCurNode.iterator();
+			while (curNodeIter.hasNext()) {
+				Integer next = curNodeIter.next();
+				if (!coverU.contains(next)) {
+					unicEdges.add(next);
+				}
+			}
+		}
+		return unicEdges;
+	}
+
 	private boolean isEquals(Set<Integer> coverU, Set<Integer> universe) {
-		if (coverU.size() != universe.size()) return false;
+		// If size of sets is not equal return false
+		if (coverU.size() != universe.size())
+			return false;
 		final Iterator<Integer> coverVer = coverU.iterator();
 		while (coverVer.hasNext()) {
 			if (!universe.contains(coverVer.next()))
